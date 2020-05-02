c926455fe36fcaad60eafe02747fafbfa3691780
hyeonsook95 <qu3230@gmail.com>
2020-05-03 01:36:00 +0900

    강의 #1 완료

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -9,4 +9,10 @@ class RoomSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Room
-        fields = ("name", "price", "bedrooms", "instant_book", "user")
+        fields = ("pk", "name", "price", "bedrooms", "instant_book", "user")
+
+
+class BigRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
--- rooms/urls.py
+++ rooms/urls.py
@@ -1,6 +1,12 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import path
-from . import views
+from . import viewsets
+
 
 app_name = "rooms"
 
-urlpatterns = [path("list/", views.list_rooms)]
+router = DefaultRouter()
+router.register("", viewsets.RoomViewSet, basename="room")
+
+
+urlpatterns = router.urls
