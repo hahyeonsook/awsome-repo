f3b7ef7df9f0ef8678ba8d4862759bc6f1b63bbd
hyeonsook95 <qu3230@gmail.com>
2020-06-04 23:25:09 +0900

    #2.14

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("list/", views.RoomsView.as_view()),
+    path("", views.RoomsView.as_view()),
+    path("search/", views.room_search),
     path("<int:pk>/", views.RoomView.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
 from .models import Room
 from .serializers import RoomSerializer
@@ -8,9 +9,14 @@ from .serializers import RoomSerializer
 
 class RoomsView(APIView):
     def get(self, request):
-        rooms = Room.objects.all()[:5]
-        serializer = RoomSerializer(rooms, many=True).data
-        return Response(serializer)
+        paginator = PageNumberPagination()
+        paginator.page_size = 20
+        rooms = Room.objects.all()
+        results = paginator.paginate_queryset(rooms, request)
+        serializer = RoomSerializer(results, many=True)
+        # 아래처럼 해주는 이유는 Response()를 해줘도 paginator가 작동하지만
+        # prev_page, next_page와 같은 기능을 사용할 수 없기 때문에 get_paginated_response()를 사용함.
+        return paginator.get_paginated_response(serializer.data)
 
     def post(self, request):
         if not request.user.is_authenticated:
@@ -22,6 +28,16 @@ class RoomsView(APIView):
             return Response(data=room_serializer, status=status.HTTP_200_OK)
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def room_search(request):
+    paginator = PageNumberPagination()
+    paginator.page_size = 10
+    rooms = Room.objects.filter()
+    results = paginator.paginate_queryset(rooms, request)
+    serialzer = RoomSerializer(results, many=True)
+    return paginator.get_paginated_response(serializer.data)
 
 
 class RoomView(APIView):
